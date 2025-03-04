e619fdb66c7b863bb525778d1db24e1ea2235b03
Andrew Stamm <andrewstamm@me.com>
2025-03-04 12:00:18 -0600

    Initial Commit

---
+++ Met Browser/Met_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Met_BrowserApp.swift
+//  Met Browser
+//
+//  Created by Andrew Stamm on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Met_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
