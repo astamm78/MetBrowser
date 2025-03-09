bf67a4579207c85930fa054df4960971c7a98eb3
Andrew Stamm <andrewstamm@me.com>
2025-03-09 15:15:58 -0500

    Preview type tests

--- Met BrowserTests/Protocols/Previewable_tests.swift
+++ Met BrowserTests/Protocols/Previewable_tests.swift
@@ -11,25 +11,31 @@ import XCTest
 final class Previewable_tests: XCTestCase {
     func test_ConstituentPreview() throws {
         XCTAssertNotNil(Constituent.preview)
+        XCTAssertTrue(type(of: Constituent.preview) == Constituent.self)
     }
     
     func test_DepartmentsResponsePreview() throws {
         XCTAssertNotNil(DepartmentsResponse.preview)
+        XCTAssertTrue(type(of: DepartmentsResponse.preview) == DepartmentsResponse.self)
     }
     
     func test_DepartmentPreview() throws {
         XCTAssertNotNil(Department.preview)
+        XCTAssertTrue(type(of: Department.preview) == Department.self)
     }
     
     func test_MetObjectPreview() throws {
         XCTAssertNotNil(MetObject.preview)
+        XCTAssertTrue(type(of: MetObject.preview) == MetObject.self)
     }
     
     func test_ObjectsResponsePreview() throws {
         XCTAssertNotNil(ObjectsResponse.preview)
+        XCTAssertTrue(type(of: ObjectsResponse.preview) == ObjectsResponse.self)
     }
     
     func test_TagPreview() throws {
         XCTAssertNotNil(Tag.preview)
+        XCTAssertTrue(type(of: Tag.preview) == Tag.self)
     }
 }
